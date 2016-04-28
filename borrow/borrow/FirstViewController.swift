98ad5129b1e4e7a087064446a3f58f100270e3fb
AniBir <anila@gmx.net>
2016-04-28 13:23:55 +0200

    deleted UINavigationController

--- borrow/borrow/FirstViewController.swift
+++ borrow/borrow/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UINavigationControllerDelegate, UINavigationController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
